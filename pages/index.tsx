044829edc8e53ea3342006effe2cfdcb6d43e199
Prithvi <prithvin19@gmail.com>
2023-07-26 23:39:48 +0530

    layout test

--- pages/index.tsx
+++ pages/index.tsx
@@ -11,6 +11,7 @@ import cloudinary from "../utils/cloudinary";
 import getBase64ImageUrl from "../utils/generateBlurPlaceholder";
 import type { ImageProps } from "../utils/types";
 import { useLastViewedPhoto } from "../utils/useLastViewedPhoto";
+import Banner from "../components/Banner";
 
 const Home: NextPage = ({ images }: { images: ImageProps[] }) => {
   const router = useRouter();
@@ -40,7 +41,7 @@ const Home: NextPage = ({ images }: { images: ImageProps[] }) => {
           content="https://nextjsconf-pics.vercel.app/og-image.png"
         />
       </Head>
-      <main className="mx-auto max-w-[1960px] p-4">
+      <main className="mx-auto p-4 pb-0  ">
         {photoId && (
           <Modal
             images={images}
@@ -49,8 +50,9 @@ const Home: NextPage = ({ images }: { images: ImageProps[] }) => {
             }}
           />
         )}
-        <div className="columns-1 gap-4 sm:columns-2 xl:columns-3 2xl:columns-4">
-          <div className="after:content relative mb-5 flex h-[629px] flex-col items-center justify-end gap-4 overflow-hidden rounded-lg bg-white/10 px-6 pb-16 pt-64 text-center text-white shadow-highlight after:pointer-events-none after:absolute after:inset-0 after:rounded-lg after:shadow-highlight lg:pt-0">
+        <Banner/>
+        <div className="flex w-screen gap-4 pt-4">
+          <div className=" after:content relative mb-5 flex max-w-[340px]  flex-col items-center justify-end gap-4 overflow-hidden rounded-lg bg-white/10 px-6 pb-16 pt-64 text-center text-white shadow-highlight after:pointer-events-none after:absolute after:inset-0 after:rounded-lg after:shadow-highlight md:w-1/3 lg:pt-0 ">
             <div className="absolute inset-0 flex items-center justify-center opacity-20">
               <span className="flex max-h-full max-w-full items-center justify-center">
                 <Bridge />
@@ -58,7 +60,7 @@ const Home: NextPage = ({ images }: { images: ImageProps[] }) => {
               <span className="absolute bottom-0 left-0 right-0 h-[400px] bg-gradient-to-b from-black/0 via-black to-black"></span>
             </div>
             {/* <Logo />/ */}
-            <h1 className="mb-4 mt-8 text-base font-bold uppercase tracking-widest">
+            <h1 className="mb-4 mt-8 pt-2 text-base font-bold uppercase tracking-widest">
               Vaara Irudhi
             </h1>
             <h1 className="mb-4 mt-8 text-base font-bold uppercase tracking-widest">
@@ -76,44 +78,89 @@ const Home: NextPage = ({ images }: { images: ImageProps[] }) => {
               Subscribe for updates
             </a>
           </div>
-          {images.map(({ id, public_id, format, blurDataUrl }) => (
-            <Link
-              key={id}
-              href={`/?photoId=${id}`}
-              as={`/p/${id}`}
-              ref={id === Number(lastViewedPhoto) ? lastViewedPhotoRef : null}
-              shallow
-              className="after:content group relative mb-5 block w-full cursor-zoom-in after:pointer-events-none after:absolute after:inset-0 after:rounded-lg after:shadow-highlight"
-            >
-              <Image
-                alt="Next.js Conf photo"
-                className="transform rounded-lg brightness-90 transition will-change-auto group-hover:brightness-110"
-                style={{ transform: "translate3d(0, 0, 0)" }}
-                placeholder="blur"
-                blurDataURL={blurDataUrl}
-                src={`https://res.cloudinary.com/${process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME}/image/upload/c_scale,w_720/${public_id}.${format}`}
-                width={720}
-                height={480}
-                sizes="(max-width: 640px) 100vw,
+          <div
+            style={{
+              overflow: "scroll",
+              // overflowY: "hidden",
+              height: "90vh",
+              // maxHeight:"500px"
+              // width:"40vw"
+            }}
+            className="scrollbar columns-1 gap-4 px-2 sm:columns-2 md:w-2/3 xl:columns-2 2xl:columns-3"
+          >
+            {images.map(({ id, public_id, format, blurDataUrl }) => (
+              <Link
+                key={id}
+                href={`/?photoId=${id}`}
+                as={`/p/${id}`}
+                ref={id === Number(lastViewedPhoto) ? lastViewedPhotoRef : null}
+                shallow
+                className="after:content group relative mb-5 block w-full cursor-zoom-in after:pointer-events-none after:absolute after:inset-0 after:rounded-lg after:shadow-highlight"
+              >
+                <Image
+                  alt="Next.js Conf photo"
+                  className="transform rounded-lg brightness-90 transition will-change-auto group-hover:brightness-110"
+                  style={{ transform: "translate3d(0, 0, 0)" }}
+                  placeholder="blur"
+                  blurDataURL={blurDataUrl}
+                  src={`https://res.cloudinary.com/${process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME}/image/upload/c_scale,w_720/${public_id}.${format}`}
+                  width={720}
+                  height={480}
+                  sizes="(max-width: 640px) 100vw,
                   (max-width: 1280px) 50vw,
                   (max-width: 1536px) 33vw,
                   25vw"
-              />
-            </Link>
-          ))}
+                />
+              </Link>
+            ))}
+            {images.map(({ id, public_id, format, blurDataUrl }) => (
+              <Link
+                key={id}
+                href={`/?photoId=${id}`}
+                as={`/p/${id}`}
+                ref={id === Number(lastViewedPhoto) ? lastViewedPhotoRef : null}
+                shallow
+                className="after:content group relative mb-5 block w-full cursor-zoom-in after:pointer-events-none after:absolute after:inset-0 after:rounded-lg after:shadow-highlight"
+              >
+                <Image
+                  alt="Next.js Conf photo"
+                  className="transform rounded-lg brightness-90 transition will-change-auto group-hover:brightness-110"
+                  style={{ transform: "translate3d(0, 0, 0)" }}
+                  placeholder="blur"
+                  blurDataURL={blurDataUrl}
+                  src={`https://res.cloudinary.com/${process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME}/image/upload/c_scale,w_720/${public_id}.${format}`}
+                  width={720}
+                  height={480}
+                  sizes="(max-width: 640px) 100vw,
+                  (max-width: 1280px) 50vw,
+                  (max-width: 1536px) 33vw,
+                  25vw"
+                />
+              </Link>
+            ))}
+          </div>
         </div>
       </main>
-      <footer className="p-6 text-center text-white/80 sm:p-12">
+      <div className="px-2 text-center text-white/80  md:w-1/3">
         Powered by{" "}
         <a
-          href="https://portfolio-2023-seven-hazel.vercel.app/"
+          href="https://webibee.com/"
           target="_blank"
-          className="font-semibold hover:text-white"
+          className="pr-2 font-semibold hover:text-white"
           rel="noreferrer"
         >
-          WebCoat
+          Webibee
         </a>
-      </footer>
+        x
+        <a
+          href="https://webibee.com/"
+          target="_blank"
+          className="pl-2 font-semibold hover:text-white"
+          rel="noreferrer"
+        >
+          SolveforX
+        </a>
+      </div>
     </>
   );
 };
